90cb9b2bfeec2a812d378454319115945891a8b2
Horácio Pedro <46960527+horacio-pedro@users.noreply.github.com>
2019-06-13 18:09:38 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require('express')
+const mongoose = require('mongoose')
 
-const app = express();
-
-app.get('/', (req, res) =>{
-    return res.send('Hello Word!');
+const app = express()
+mongoose.connect('mongodb+srv://confit-dev:HoracioDeveloper@cluster0-vusb6.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
 })
+
+app.use(require('./routes'))
 
 app.listen(3333);
